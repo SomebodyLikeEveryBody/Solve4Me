18d91aa4c67a2b43b30ff7cffd5ae3464503690d
r0b3rt[0] <nullepart@rien.com>
2020-11-06 18:04:07 +0100

    latex to mathml management

--- js.js
+++ js.js
@@ -1,14 +1,43 @@
+
+/*
+ * textToMathML():
+ * Takes a string and return a <math> DOM object corresponding to the expression
+ * Uses the M.sToMathE() function in jqmath.js
+ * */
+function texToMathML(pTexStr) {
+    return (M.sToMathE(pTexStr, true));
+}
+
+function getAllStatements(pStr)
+{
+    return pStr.split('\n')
+}
+
+/*
+ * MAIN
+ * */
 $(function () {
     $('textarea#input').focus();
 
     $('button#do_solve').click(() => {
         if ($('textarea#input').val() !== '') {
-            $('section#output').html('<math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><mrow><mi>J</mi><mo stretchy="false">(</mo><mi>&#x3B8;</mi><mo stretchy="false">)</mo><mo>=</mo><mfrac><mn>1</mn><mrow><mn>2</mn><mi>m</mi></mrow></mfrac><mo stretchy="false">[</mo><munderover><mo data-mjx-texclass="OP">&#x2211;</mo><mrow><mi>i</mi><mo>=</mo><mn>1</mn></mrow><mi>m</mi></munderover><mo stretchy="false">(</mo><msub><mi>h</mi><mi>&#x3B8;</mi></msub><mo stretchy="false">(</mo><msup><mi>x</mi><mrow><mo stretchy="false">(</mo><mi>i</mi><mo stretchy="false">)</mo></mrow></msup><mo stretchy="false">)</mo><mo>&#x2212;</mo><msup><mi>y</mi><mrow><mo stretchy="false">(</mo><mi>i</mi><mo stretchy="false">)</mo></mrow></msup><mo stretchy="false">)</mo><mn>2</mn><mo>+</mo><mi>&#x3BB;</mi><munderover><mo data-mjx-texclass="OP">&#x2211;</mo><mrow><mi>j</mi><mo>=</mo><mn>1</mn></mrow><mi>n</mi></munderover><msubsup><mi>&#x3B8;</mi><mi>j</mi><mn>2</mn></msubsup></mrow></math>')
+            $('section#output').html('');
+
+            let statements = getAllStatements($('textarea#input').val());
+            let appened_element = null;
+            for (statement of statements) {
+                appened_element = $('<div class="formula"></div>');
+                appened_element.append($(texToMathML(statement)));
+                appened_element.hide(0);
+
+                $('section#output').append(appened_element);
+                appened_element.animate({width: 'toggle'}, 200);
+            }
+                
         }
     });
 
     $('button#looper').focus(() => {
         $('textarea#input').focus();
-    });
-
+    })
 });
